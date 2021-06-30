07b0aa5ae2725a96104ce38956bd0143b515a8fa
Joao-Nunes-Zup <joao.nunes@zup.com>
2021-06-30 15:29:12 -0300

    impedindo a criação de dois registro com mesmo endereço de e-mail

--- src/main/java/com/bootcamp/casadocodigo/controller/AutorController.java
+++ src/main/java/com/bootcamp/casadocodigo/controller/AutorController.java
@@ -4,8 +4,10 @@ import com.bootcamp.casadocodigo.dto.NovoAutorRequest;
 import com.bootcamp.casadocodigo.dto.NovoAutorResponse;
 import com.bootcamp.casadocodigo.model.Autor;
 import com.bootcamp.casadocodigo.repository.AutorRepository;
+import com.bootcamp.casadocodigo.validator.ValidaDuplicidadeDeEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,6 +18,14 @@ public class AutorController {
 
     @Autowired
     private AutorRepository autorRepository;
+
+    @Autowired
+    private ValidaDuplicidadeDeEmail validadaDuplicidadeDeEmail;
+
+    @InitBinder
+    public void init(WebDataBinder binder) {
+        binder.addValidators(validadaDuplicidadeDeEmail);
+    }
 
     @PostMapping
     public ResponseEntity<?> criar(@RequestBody @Valid NovoAutorRequest autorRequest) {
--- src/main/java/com/bootcamp/casadocodigo/repository/AutorRepository.java
+++ src/main/java/com/bootcamp/casadocodigo/repository/AutorRepository.java
@@ -4,6 +4,10 @@ import com.bootcamp.casadocodigo.model.Autor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AutorRepository extends CrudRepository<Autor, Long> {
+
+    Optional<Autor> findByEmail(String email);
 }
